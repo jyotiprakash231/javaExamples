293c0586a6deac889fffa40259ba5e567fbb75e0
Jyoti Prakash <jpsahoo.edu@gmail.com>
2019-09-25 14:52:27 +0530

    nothing

--- src/javaExampleProject/ArrayListDemo.java
+++ src/javaExampleProject/ArrayListDemo.java
@@ -9,6 +9,9 @@ public class ArrayListDemo {
 		list.add("A");
 		list.add(2,null);
 		list.add(2, "hi");
+		list.add(10);
+		boolean b=list.add("hi how are you");
+		System.out.println(b);
 		System.out.println(list.size());
 		System.out.println(list);
 		list.add(3);
