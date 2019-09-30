363593acfe85fb3c6407e87806c96b2e01d47f8f
Jyoti Prakash <jpsahoo.edu@gmail.com>
2019-09-30 12:32:19 +0530

    added another string demo

---
+++ src/javaExampleProject/StringDemo/StringConcatenationDemo.java
@@ -0,0 +1,17 @@
+package javaExampleProject.StringDemo;
+
+public class StringConcatenationDemo {
+
+	public static void main(String[] args) {
+		String s=new String("Hii ");
+		String s2="How are you";
+		System.out.println(s+s2+" ?");
+		String LongText="hi how are you ? "+
+						"my name is jyoti. "+
+						"Bye";
+		System.out.println(LongText);
+		System.out.println("Four="+2+2);
+		System.out.println("Four="+(2+2));
+	}
+
+}
